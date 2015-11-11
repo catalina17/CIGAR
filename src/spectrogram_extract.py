c7ecbaa8aa1de3d92146d3d74e8b6e4e70b1ffd4
Catalina <catalina.cangea@gmail.com>
2015-11-11 16:11:04 +0000

    Added spectrogram extraction

---
+++ src/spectrogram_extract.py
@@ -0,0 +1,47 @@
+import librosa
+import numpy
+import os
+import pylab
+
+
+def spectrograms_for_genre(genre_name):
+    """
+    Extract spectrograms of audio files belonging to the given genre.
+
+    Parameters
+    ----------
+    genre_name : str
+        The name of the genre we wish to extract spectrograms for
+    """
+
+    # Create directory, if not already existent
+    dir_path = os.path.abspath(os.path.join(os.getcwd(), '../../spectrograms/', genre_name))
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    # Produce a spectrogram for each audio file
+    for song_index in range(0, 100):
+        filepath = '000' + str(song_index) + '.wav'
+        if song_index < 10:
+            filepath = '0' + filepath
+        filepath = genre_name + '/' + genre_name + '.' + filepath
+
+        # Extract raw representation of signal from audio file
+        audio_time_series, sampling_rate = librosa.load(
+            os.path.abspath(os.path.join(os.getcwd(), '../../genres/', filepath)))
+
+        # Produce the spectrogram with 128 frequency bins, FFT window of 2048 samples
+        spectrogram = librosa.feature.melspectrogram(y=audio_time_series, sr=sampling_rate,
+                                                     n_mels=128, n_fft=2048, fmax=8000)
+
+        # Save the spectrogram image
+        librosa.display.specshow(librosa.logamplitude(spectrogram, ref_power=numpy.max), fmax=8000)
+        pylab.savefig(os.path.abspath(os.path.join(os.getcwd(), '../../spectrograms/',
+                                                   filepath[:-3] + 'png')))
+
+
+if __name__ == '__main__':
+    genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae',
+              'rock']
+    for genre in genres:
+        spectrograms_for_genre(genre)

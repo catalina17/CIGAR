30fc3295c5146f1938f897dad2fdac148075412a
Catalina <catalina.cangea@gmail.com>
2015-12-01 14:23:02 +0000

    Some layer forward-prop

--- src/convnet/conv_layer.py
+++ src/convnet/conv_layer.py
@@ -17,12 +17,18 @@ class ConvLayer(Layer):
         """
         self.num_filters = num_filters
         self.filter_shape = filter_shape
-        self.filter_weights = np.random.rand(filter_shape[0], filter_shape[1])
 
-        self.d_weights = np.zeros(filter_shape)
+        self.filter_weights = np.ndarray(num_filters, dtype=np.ndarray)
+        for i in range(num_filters):
+            self.filter_weights[i] = np.random.rand(filter_shape[0], filter_shape[1])
+        print "Weights: "
+        print self.filter_weights
+
+        self.d_weights = [np.zeros(filter_shape)]
         self.weight_decay = weight_decay
 
         if padding_mode:
+            # Padding along height dimension
             self.num_padding_zeros = 2 * (filter_shape[1] - 1)
         else:
             self.num_padding_zeros = 0
@@ -30,7 +36,25 @@ class ConvLayer(Layer):
         self.input_shape = None
 
     def forward_prop(self, input):
-        raise NotImplementedError()
+        assert self.input_shape == input.shape, "Input does not have correct shape"
+
+        padded_input = np.zeros((self.input_shape[0], self.input_shape[1] + self.num_padding_zeros))
+        padded_input[0:self.input_shape[0],
+                     self.num_padding_zeros / 2:
+                     self.num_padding_zeros / 2 + self.input_shape[1]] = input
+
+        output = np.zeros(self.get_output_shape())
+
+        filter_h = self.filter_shape[0]
+        filter_w = self.filter_shape[1]
+        range_i = self.get_output_shape()[1] - filter_w + 1
+
+        for f in range(self.num_filters):
+            for i in range(range_i):
+                output[f][i] = sum(sum(np.multiply(self.filter_weights[f],
+                                                   padded_input[0:filter_h, i:i+filter_w])))
+
+        return output
 
     def back_prop(self, output_grad):
         raise NotImplementedError()
@@ -61,3 +85,16 @@ class ConvLayer(Layer):
 
     def update_parameters(self, learning_rate):
         raise NotImplementedError()
+
+if __name__ == "__main__":
+
+    layer = ConvLayer(5, (4, 4), 0.01, True)
+    layer.set_input_shape((8, 8))
+
+    dummy_input = np.random.randn(8, 8)
+    print "Dummy input: "
+    print dummy_input
+
+    res = layer.forward_prop(dummy_input)
+    print "Output of Conv layer: " + str(res.shape)
+    print res
--- src/convnet/maxpooling_layer.py
+++ src/convnet/maxpooling_layer.py
@@ -1,4 +1,5 @@
 from layer import Layer
+import numpy as np
 
 
 class MaxPoolingLayer(Layer):
@@ -14,7 +15,37 @@ class MaxPoolingLayer(Layer):
         self.input_shape = None
 
     def forward_prop(self, input):
-        raise NotImplementedError()
+        assert self.input_shape == input.shape, "Input does not have correct shape"
+
+        if (len(self.get_output_shape()) == 1):
+            output = np.zeros((1, self.get_output_shape()[0]))
+        else:
+            output = np.zeros(self.get_output_shape())
+
+        filter_h = self.filter_shape[0]
+        filter_w = self.filter_shape[1]
+        input_h = self.input_shape[0]
+        input_w = self.input_shape[1]
+
+        range_i = input_h/filter_h
+        range_j = input_w/filter_w
+
+        for i in range(range_i):
+            for j in range(range_j):
+                current_h = i * filter_h
+                current_w = j * filter_w
+
+                max_val = -np.inf
+                for h in range(filter_h):
+                    for w in range(filter_w):
+                        max_val = max(max_val, input[current_h + h][current_w + w])
+
+                output[i, j] = max_val
+
+        if output.shape[0] == 1:
+            return output[0]
+        else:
+            return output
 
     def back_prop(self, output_grad):
         raise NotImplementedError()
@@ -62,3 +93,15 @@ class MaxPoolingLayer(Layer):
 
         """
         raise NotImplementedError()
+
+if __name__ == "__main__":
+    dummy_input = np.array([[1.4326, 2.472634, -26.4376],
+                            [-5476.436, 49.0, 0.467235]])
+    print dummy_input
+
+    maxpool_layer = MaxPoolingLayer(filter_shape=(1,3))
+    maxpool_layer.set_input_shape(dummy_input.shape)
+
+    res = maxpool_layer.forward_prop(dummy_input)
+    print "Output of MaxPool layer: " + str(res.shape)
+    print res

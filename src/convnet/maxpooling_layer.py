42938f01cf5920a9b069c59e3129a332764ae5fc
Catalina <catalina.cangea@gmail.com>
2015-12-04 12:22:58 +0000

    Fwd/backprop in maxpooling layer

--- src/convnet/maxpooling_layer.py
+++ src/convnet/maxpooling_layer.py
@@ -14,6 +14,7 @@ class MaxPoolingLayer(Layer):
         self.filter_shape = filter_shape
         print "Filter shape: " + str(filter_shape)
         self.input_shape = None
+        self.max_activation_indices = None
 
     def forward_prop(self, input):
         assert self.input_shape == input.shape, "Input does not have correct shape"
@@ -22,6 +23,8 @@ class MaxPoolingLayer(Layer):
             output = np.empty((1, self.get_output_shape()[0]))
         else:
             output = np.empty(self.get_output_shape())
+
+        self.max_activation_indices = np.empty(self.get_output_shape() + (2,))
 
         filter_h = self.filter_shape[0]
         filter_w = self.filter_shape[1]
@@ -36,8 +39,18 @@ class MaxPoolingLayer(Layer):
                 current_h = i * filter_h
                 current_w = j * filter_w
 
-                output[i, j] = input[current_h:current_h + filter_h,
-                                     current_w:current_w + filter_w].max(axis=0).max(axis=0)
+                max_val = -np.inf
+                h_max = 0
+                w_max = 0
+                for h in range(current_h, current_h + filter_h):
+                    for w in range(current_w, current_w + filter_w):
+                        if input[h, w] > max_val:
+                            h_max = h
+                            w_max = w
+                            max_val = input[h, w]
+                output[i, j] = max_val
+                self.max_activation_indices[i, j, 0] = h_max
+                self.max_activation_indices[i, j, 1] = w_max
 
         if output.shape[0] == 1:
             return output[0]
@@ -45,7 +58,15 @@ class MaxPoolingLayer(Layer):
             return output
 
     def back_prop(self, output_grad):
-        raise NotImplementedError()
+        input_grad = np.zeros(self.input_shape)
+
+        for i in range(output_grad.shape[0]):
+            for j in range(output_grad.shape[1]):
+                h = self.max_activation_indices[i, j, 0]
+                w = self.max_activation_indices[i, j, 1]
+                input_grad[h][w] = output_grad[i][j]
+
+        return input_grad
 
     def set_input_shape(self, shape):
         """
@@ -82,11 +103,21 @@ class MaxPoolingLayer(Layer):
         return shape
 
 if __name__ == "__main__":
-    dummy_input = np.random.randn(2, 4)
+    dummy_input = np.random.randn(4, 4)
+    print "Input: "
     print dummy_input
 
     layer = MaxPoolingLayer(filter_shape=(2,2))
     layer.set_input_shape(dummy_input.shape)
 
+    print "Forward propagation:"
     res = layer.forward_prop(dummy_input)
     print res
+    print '\n'
+
+    dummy_output_grad = np.ones((2, 2))
+    print "Output gradient: "
+    print dummy_output_grad
+
+    print "Backpropagation:"
+    print layer.back_prop(dummy_output_grad)

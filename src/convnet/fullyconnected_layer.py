ba7e59fc1d64a4337defd39f544a6454bb4f1f2b
Catalina <catalina.cangea@gmail.com>
2015-12-24 10:58:08 +0200

    Backprop + param update in FC layer

--- src/convnet/fullyconnected_layer.py
+++ src/convnet/fullyconnected_layer.py
@@ -14,10 +14,13 @@ class FullyConnectedLayer(Layer):
         """
         self.num_nodes = num_nodes
         self.biases = np.zeros(num_nodes)
+        self.d_biases = np.empty(num_nodes)
 
         self.input_shape = None
         self.current_input = None
+
         self.weights = None
+        self.d_weights = None
 
     def forward_prop(self, input):
         assert self.input_shape == input.shape, "Input does not have correct shape"
@@ -25,6 +28,16 @@ class FullyConnectedLayer(Layer):
         return np.dot(input, self.weights) + self.biases
 
     def back_prop(self, output_grad):
+        self.d_weights = np.dot(np.resize(self.current_input, (self.input_shape[0], 1)),
+                                np.resize(output_grad, (output_grad.shape[0], 1)).T)
+
+        print "Weights derivative: "
+        print self.d_weights
+        self.d_biases = output_grad
+        print "Biases derivative: "
+        print self.d_biases
+
+        print "Propagated gradient: "
         return np.dot(output_grad, self.weights.T)
 
     def set_input_shape(self, shape):
@@ -38,6 +51,8 @@ class FullyConnectedLayer(Layer):
         self.input_shape = shape
         self.weights = np.random.randn(shape[0], self.num_nodes)
         print "FullyConnectedLayer with input shape " + str(shape)
+        print "Weights: "
+        print self.weights
 
     def get_output_shape(self):
         """
@@ -59,7 +74,8 @@ class FullyConnectedLayer(Layer):
         learning_rate : float
 
         """
-        raise NotImplementedError()
+        self.weights -= learning_rate * self.d_weights
+        self.biases -= learning_rate * self.d_biases
 
 if __name__ == "__main__":
     dummy_input = np.ones((4,))
@@ -69,15 +85,21 @@ if __name__ == "__main__":
     layer = FullyConnectedLayer(5)
     layer.set_input_shape((4,))
 
-    print "Forward propagation:"
+    print "\n--->> Forward propagation:"
     print sum(layer.weights)
     print layer.forward_prop(dummy_input)
-    print '\n'
 
-    dummy_output_grad = np.ones((5,))
-    print "Output gradient: "
+    dummy_output_grad = np.random.randn(5)
+    print "\nOutput gradient: "
     print dummy_output_grad
 
-    print "Backpropagation: "
-    print sum(layer.weights.T)
+    print "\n--->> Backpropagation: "
     print layer.back_prop(dummy_output_grad)
+
+    print "\n--->> Params before update: "
+    print layer.weights
+    print layer.biases
+    layer.update_parameters(0.01)
+    print "\n--->> Params after update: "
+    print layer.weights
+    print layer.biases

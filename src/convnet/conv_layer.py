Weight scaling as param in layers; fixed global pooling

src/convnet/conv_layer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class ConvLayer(Layer):
 
-    def __init__(self, num_filters, filter_shape, weight_decay, padding_mode=True):
+    def __init__(self, num_filters, filter_shape, weight_decay, weight_scale, padding_mode=True):
         """
 
         Parameters
@@ -12,6 +12,7 @@ class ConvLayer(Layer):
         num_filters : int
         filter_shape : tuple
         weight_decay : float
+        weight_scale : float
         padding_mode : bool
 
         """
@@ -21,7 +22,7 @@ class ConvLayer(Layer):
 
         self.filter_weights = np.empty((num_filters, filter_shape[0], filter_shape[1]))
         for i in range(num_filters):
-            self.filter_weights[i] = np.random.normal(scale=0.1, size=filter_shape)
+            self.filter_weights[i] = np.random.normal(scale=weight_scale, size=filter_shape)
             # print "Filter weights for filter " + str(i + 1) + ":\n", self.filter_weights[i]
         self.d_filter_weights = np.zeros(self.filter_weights.shape)
 
@@ -73,10 +74,10 @@ class ConvLayer(Layer):
 
         for f in range(self.num_filters):
             self.d_filter_weights[f] += self.weight_decay * self.filter_weights[f]
-            print "Weight derivatives for filter " + str(f + 1) + ":\n", self.d_filter_weights[f]
+            # print "Weight derivatives for filter " + str(f + 1) + ":\n", self.d_filter_weights[f]
 
         self.d_biases += np.sum(output_grad, axis=1)
-        print "Biases derivatives:\n", self.d_biases
+        # print "Biases derivatives:\n", self.d_biases
 
         return padded_input_grad[:, filter_w - 1:range_w]
 
@@ -115,7 +116,8 @@ if __name__ == "__main__":
     dummy_input = np.ones((4, 8))
     print "Input:\n", dummy_input
 
-    layer = ConvLayer(num_filters=3, filter_shape=(4, 3), weight_decay=0.1, padding_mode=True)
+    layer = ConvLayer(num_filters=3, filter_shape=(4, 3), weight_decay=0.1, weight_scale=0.01,
+                      padding_mode=True)
     layer.set_input_shape((4, 8))
 
     print "\n--->> Forward propagation:\n", layer.forward_prop(dummy_input)

src/convnet/fullyconnected_layer.py
@@ -4,12 +4,14 @@ import numpy as np
 
 class FullyConnectedLayer(Layer):
 
-    def __init__(self, num_nodes, weight_decay):
+    def __init__(self, num_nodes, weight_decay, weight_scale):
         """
 
         Parameters
         ----------
         num_nodes : int
+        weight_decay : float
+        weight_scale : float
 
         """
         self.num_nodes = num_nodes
@@ -17,6 +19,7 @@ class FullyConnectedLayer(Layer):
         self.d_biases = np.zeros(num_nodes)
 
         self.weight_decay = weight_decay
+        self.weight_scale = weight_scale
 
         self.input_shape = None
         self.current_input = None
@@ -33,12 +36,12 @@ class FullyConnectedLayer(Layer):
         self.d_weights += np.dot(np.resize(self.current_input, (self.input_shape[0], 1)),
                                  np.resize(output_grad, (output_grad.shape[0], 1)).T) +\
                           self.weight_decay * self.weights
-        print "Weights derivative:\n", self.d_weights
+        # print "Weights derivative:\n", self.d_weights
 
         self.d_biases += output_grad
-        print "Biases derivative:\n", self.d_biases
+        # print "Biases derivative:\n", self.d_biases
 
-        print "Propagated gradient: "
+        # print "Propagated gradient: "
         return np.dot(output_grad, self.weights.T)
 
     def set_input_shape(self, shape):
@@ -50,7 +53,7 @@ class FullyConnectedLayer(Layer):
 
         """
         self.input_shape = shape
-        self.weights = np.random.normal(scale=0.1, size=(shape[0], self.num_nodes))
+        self.weights = np.random.normal(self.weight_scale, size=(shape[0], self.num_nodes))
         self.d_weights = np.zeros(self.weights.shape)
 
         print "FullyConnectedLayer with input shape " + str(shape)
@@ -86,7 +89,7 @@ if __name__ == "__main__":
     dummy_input = np.ones((4,))
     print "Input:\n", dummy_input
 
-    layer = FullyConnectedLayer(5)
+    layer = FullyConnectedLayer(5, weight_decay=0.1, weight_scale=0.01)
     layer.set_input_shape((4,))
 
     print "\n--->> Forward propagation:\n", sum(layer.weights), "\n", layer.forward_prop(dummy_input)

src/convnet/globalpooling_layer.py
@@ -17,13 +17,13 @@ class GlobalPoolingLayer(Layer):
         self.max_activation_indices = np.empty(self.get_output_shape())
 
         for f in range(self.input_shape[0]):
-            # Mean pooling
-            output[f] = np.mean(input[f])
+            # Average pooling
+            output[f] = np.sum(input[f])
             # Max pooling
             output[self.input_shape[0] + f] = np.max(input[f])
             self.max_activation_indices[f] = np.argmax(input[f])
-            # L2 pooling
-            output[2 * self.input_shape[0] + f] = np.sqrt(sum(input[f] ** 2))
+            # L2-norm pooling
+            output[2 * self.input_shape[0] + f] = np.sum(input[f] ** 2)
         return output
 
     def back_prop(self, output_grad):
@@ -34,13 +34,12 @@ class GlobalPoolingLayer(Layer):
         input_grad = np.empty(self.input_shape)
         range_f = self.input_shape[0]
         for f in range(range_f):
-            mean_input_grad = mean_output_grad[f] / self.input_shape[1]
-            max_input_grad = np.zeros(self.input_shape[1])
-            max_input_grad[self.max_activation_indices[f]] = max_output_grad[f]
-            l2_input_grad = self.current_input[f] / np.sqrt(sum(self.current_input[f] ** 2)) *\
-                            l2_output_grad[f]
-
-            input_grad[f] = mean_input_grad + max_input_grad + l2_input_grad
+            # Average grad
+            input_grad[f] = mean_output_grad[f]
+            # Max grad
+            input_grad[f, self.max_activation_indices[f]] += max_output_grad[f]
+            # L2-norm grad
+            input_grad[f] += 2 * self.current_input[f] * l2_output_grad[f]
 
         return input_grad
 

src/convnn.py
@@ -23,6 +23,27 @@ class ConvNN(object):
         self.layers = layers
         self.data_provider = data_provider
 
+    def _setup_layers(self, cnn_input_shape, cnn_output_shape):
+        """
+
+        Parameters
+        ----------
+        cnn_input_shape : tuple
+        cnn_output_shape : tuple
+
+        """
+        current_shape = cnn_input_shape
+        for layer in self.layers:
+            layer.set_input_shape(current_shape)
+            current_shape = layer.get_output_shape()
+            print "DONE setting up " + str(type(layer)) + '\n'
+
+        assert current_shape == cnn_output_shape, "Computed output shape " + str(current_shape) +\
+                                                  " does not match given output shape " +\
+                                                  str(cnn_output_shape)
+
+        print "ConvNN setup successful!"
+
     def train(self, learning_rate, num_iters):
         """
 
@@ -63,26 +84,11 @@ class ConvNN(object):
 
                 batch = self.data_provider.get_next_batch()
 
-    def _setup_layers(self, cnn_input_shape, cnn_output_shape):
-        """
+    def error(self, training_batch):
+        raise NotImplementedError()
 
-        Parameters
-        ----------
-        cnn_input_shape : tuple
-        cnn_output_shape : tuple
-
-        """
-        current_shape = cnn_input_shape
-        for layer in self.layers:
-            layer.set_input_shape(current_shape)
-            current_shape = layer.get_output_shape()
-            print "DONE setting up " + str(type(layer)) + '\n'
-
-        assert current_shape == cnn_output_shape, "Computed output shape " + str(current_shape) +\
-                                                  " does not match given output shape " +\
-                                                  str(cnn_output_shape)
-
-        print "ConvNN setup successful!"
+    def training_loss(self, training_batch):
+        raise NotImplementedError()
 
     def predict(self, input):
         """
@@ -105,10 +111,6 @@ class ConvNN(object):
         predicted_class = np.argmax(current_input)
         return predicted_class
 
-    def _gradient_check(self, input, output):
-        raise NotImplementedError()
-
-
 if __name__ == '__main__':
     neural_net = ConvNN([ConvLayer(256, (128, 4), 0.1, False),
                          ActivationLayer('ReLU'),
@@ -126,18 +128,30 @@ if __name__ == '__main__':
                          ActivationLayer('ReLU'),
                          GlobalPoolingLayer(),
 
-                         FullyConnectedLayer(2048, 0.1),
-                         FullyConnectedLayer(2048, 0.1),
-                         FullyConnectedLayer(40, 0.1),
-                         FullyConnectedLayer(2, 0.1),
+                         FullyConnectedLayer(2048, weight_decay=0.1, weight_scale=0.01),
+                         FullyConnectedLayer(2048, weight_decay=0.1, weight_scale=0.01),
+                         FullyConnectedLayer(40, weight_decay=0.1, weight_scale=0.01),
+                         FullyConnectedLayer(2, weight_decay=0.1, weight_scale=0.01),
                          SoftmaxLayer()],
                         DataProvider(20))
 
     neural_net._setup_layers((128, 599), (2, ))
 
     dummy_input = np.random.uniform(0, 255, [128, 599])
+    # dummy_input /= np.sum(dummy_input)
+    print "CNN input:\n", dummy_input
+
     for layer in neural_net.layers:
         dummy_input = layer.forward_prop(dummy_input)
-        # if isinstance(layer, FullyConnectedLayer) or isinstance(layer, SoftmaxLayer):
-        print str(type(layer))
+        print "LAYER--->>:", str(type(layer))
         print dummy_input
+
+    print "\n--->>BACKPROPAGATION\n"
+    dummy_output = np.array([1, 0])
+    current_gradient = neural_net.layers[-1].initial_gradient(dummy_input, dummy_output)
+    print "\nInitial gradient:\n", current_gradient
+
+    for layer in reversed(neural_net.layers[:-1]):
+        current_gradient = layer.back_prop(current_gradient)
+        print "LAYER--->>:", str(type(layer))
+        print current_gradient
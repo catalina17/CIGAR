6131192f8ab74ecede98150e5981d30a93a68b18
Catalina <catalina.cangea@gmail.com>
2016-03-28 17:00:05 +0100

    Docstrings for math

--- src/convnet/util/math.py
+++ src/convnet/util/math.py
@@ -2,19 +2,72 @@ import numpy as np
 
 
 def sigmoid(x):
+    """
+    sigmoid(x) = 1 / (1 + e^(-x))
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the sigmoid function to each element of the input.
+
+    """
     return 1.0 / (1.0 + np.exp(-x))
 
 
 def d_sigmoid(x):
+    """
+    sigmoid'(x) = sigmoid(x) * (1 - sigmoid(x))
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the derivative of the sigmoid function to each element of the input.
+
+    """
     s = sigmoid(x)
     return s * (1 - s)
 
 
 def relu(x):
+    """
+    ReLU(x) = max(0, x)
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the ReLU function to each element of the input.
+
+    """
     return np.maximum(0, x)
 
 
 def d_relu(x):
+    """
+    ReLU'(x) = { 1, if x > 0
+               { 0, otherwise
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the derivative of the ReLU function to each element of the input.
+
+    """
     res = np.zeros(x.shape)
     res[x > 0] = 1
 
@@ -22,6 +75,20 @@ def d_relu(x):
 
 
 def leaky_relu(x):
+    """
+    leakyReLU(x) = { x       , if x > 0
+                   { 0.01 * x, otherwise
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the leakyReLU function to each element of the input.
+
+    """
     res = x
     res[x <= 0] *= 0.01
 
@@ -29,6 +96,21 @@ def leaky_relu(x):
 
 
 def d_leaky_relu(x):
+    """
+    leakyReLU'(x) = { 1   , if x > 0
+                    { 0.01, otherwise
+
+    Parameters
+    ----------
+    x : scalar or array
+
+    Returns
+    -------
+    scalar or array
+        The result of applying the derivative of the leakyReLU function to each element of the
+        input.
+
+    """
     res = np.zeros(x.shape)
     res[x > 0] = 1
     res[x <= 0] = 0.01
